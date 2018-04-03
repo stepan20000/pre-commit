5b304584f1522809090e9c7e017fe4d7b876f679
stepan_yefimov <stepan.yefimov@gmail.com>
2018-04-03 13:24:42 +0300

    feat(MOB-555): My correct commit message

--- pre-commit-script.js
+++ pre-commit-script.js
@@ -1,6 +1,7 @@
 console.log('pre-commit-script.js');
 const fs = require('fs');
 let commitMessage;
+let regExp = /(feat|fix|style|refactor|chore)\(MOB-\d{3,4}\): .{10,1000}/gi;
 
 // print process.argv
 // process.argv.forEach(function (val, index, array) {
@@ -16,17 +17,20 @@ let commitMessage;
 //     }
 // });
 
+const removeComments = (str) => str.split(/\r?\n/).filter((line) => line[0] == '#').join(' ');
 
 try {
     commitMessage = fs.readFileSync(process.argv[2], 'utf8');
+    console.log('Commit message is:');
     console.log(commitMessage);
-    if (commitMessage.length > 10) {
-        throw new Error('Very long message. Sorry.')
+    console.log('Commit message without comments: ')
+    console.log(removeComments(commitMessage));
+    if (!removeComments(commitMessage).match(re)) {
+        throw new Error('Your commit message does not match to the pattern. \n' +
+            'Please read https://github.com/Debtdomain/PCS-Boardportal/wiki/Working-with-git \n' +
+            'and try again');
     }
 } catch(e) {
-    console.log('Error:', e.stack);
+    console.log(e.stack);
     process.exit(1);
 }
-
-console.log('Commit message is:');
-console.log(commitMessage);
