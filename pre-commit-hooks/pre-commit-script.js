e0791d016fb11cb676bb2c7b952a86414359f513
stepan_yefimov <stepan.yefimov@gmail.com>
2018-04-03 16:26:55 +0300

    feat(MOB-555): Everything is ready

--- pre-commit-hooks/pre-commit-script.js
+++ pre-commit-hooks/pre-commit-script.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
+
+const newLineSymbol = process.platform == 'win32' ? '\r' : '\n';
+const removeComments = (str) => str.split(/\r?\n/).filter((line) => line[0] != '#').join(newLineSymbol);
 let regExp = /(feat|fix|style|refactor|chore)\(MOB-\d{3,4}\): .{10,1000}/gi;
 
-const removeComments = (str) => str.split(/\r?\n/).filter((line) => line[0] != '#').join(' ');
-
-console.log(' *** Pre Commit Hook *** ');
+console.log(' ***   Pre Commit Hook   *** ');
 
 try {
     let commitMessage = fs.readFileSync(process.argv[2], 'utf8');
+    console.log('Commit message');
+    console.log(commitMessage);
+    console.log('Without comments');
+    console.log(removeComments(commitMessage));
 
     if (!removeComments(commitMessage).match(regExp)) {
         throw new Error('Your commit message does not match to the pattern. \n' +
