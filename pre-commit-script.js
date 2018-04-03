36aebd508230b3db9892cbbf0185e1d7b61c4ee3
stepan_yefimov <stepan.yefimov@gmail.com>
2018-04-03 13:34:04 +0300

    feat(MOB-000): My correct commit message

--- pre-commit-script.js
+++ pre-commit-script.js
@@ -1,31 +1,14 @@
-console.log('pre-commit-script.js');
 const fs = require('fs');
-let commitMessage;
 let regExp = /(feat|fix|style|refactor|chore)\(MOB-\d{3,4}\): .{10,1000}/gi;
 
-// print process.argv
-// process.argv.forEach(function (val, index, array) {
-//     console.log(index + ': ' + val);
-// });
+const removeComments = (str) => str.split(/\r?\n/).filter((line) => line[0] != '#').join(' ');
 
-// fs.readFile(process.argv[2], 'utf8', function (err,data) {
-//     if (err) {
-//         return console.log('Error in pre-commit-script.js');
-//     } else {
-//         commitMessage = data;
-//         console.log(data);
-//     }
-// });
-
-const removeComments = (str) => str.split(/\r?\n/).filter((line) => line[0] == '#').join(' ');
+console.log(' *** Pre Commit Hook *** ');
 
 try {
-    commitMessage = fs.readFileSync(process.argv[2], 'utf8');
-    console.log('Commit message is:');
-    console.log(commitMessage);
-    console.log('Commit message without comments: ')
-    console.log(removeComments(commitMessage));
-    if (!removeComments(commitMessage).match(re)) {
+    let commitMessage = fs.readFileSync(process.argv[2], 'utf8');
+
+    if (!removeComments(commitMessage).match(regExp)) {
         throw new Error('Your commit message does not match to the pattern. \n' +
             'Please read https://github.com/Debtdomain/PCS-Boardportal/wiki/Working-with-git \n' +
             'and try again');
