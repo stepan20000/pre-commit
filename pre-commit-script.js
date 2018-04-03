3f4bb504f1eb3e2548eef62262efd7e15780847c
stepan_yefimov <stepan.yefimov@gmail.com>
2018-04-03 10:36:03 +0300

    My super commit message

--- pre-commit-script.js
+++ pre-commit-script.js
@@ -1,6 +1,19 @@
 console.log('pre-commit-script.js');
+const fs = require('fs');
+let commitMessage;
 
 // print process.argv
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
+// process.argv.forEach(function (val, index, array) {
+//     console.log(index + ': ' + val);
+// });
+
+fs.readFile(process.argv[2], 'utf8', function (err,data) {
+    if (err) {
+        return console.log('Error in pre-commit-script.js');
+    } else {
+        commitMessage = data;
+    }
 });
+
+console.log('Commit message is:');
+console.log(commitMessage);
